0ddd373024da5c6e44b2a2ccb2c301659ad55e15
drpyser <schok53@gmail.com>
2019-03-07 00:20:50 -0500

    Fixed race to wait for all tasks to finish before returning

--- concurrency/utils.py
+++ concurrency/utils.py
@@ -38,9 +38,16 @@ def asynchronous(settings: object=DefaultAsynchronousSettings()):
 
 
 async def race(aws: Iterable[Awaitable], timeout=None):
-    done, pending = await asyncio.wait(aws, timeout=timeout)
+    done, pending = await asyncio.wait(aws, timeout=timeout, return_when=asyncio.FIRST_COMPLETED)
     for t in pending:
         t.cancel()
+    else:
+        # wait for all tasks to finish, i.e. for cancellation to propagate
+        await asyncio.wait(aws, return_when=asyncio.ALL_COMPLETED)
+    assert all(t.done() for t in pending), str(pending)
+    assert all(t.done() for t in done)
+    #assert len(done) <= 1
+    # note: there may be more than 1 task in done, but we only return one "winner"
     return done.pop()
 
 
@@ -59,185 +66,6 @@ import logging
 
 
 #async def propagator(t1: asyncio.Task, t2: asyncio.Task, callback: Callable=None):
-
-import abc
-import types
-from typing import FrozenSet
-
-
-class TaskManager(abc.ABC):
-    tasks: FrozenSet[asyncio.Task]
-
-    @abc.abstractmethod
-    def spawn(self, coro: types.CoroutineType) -> asyncio.Task:
-        """
-        spawn a task managed by this TaskManager
-        """
-        pass
-
-    @abc.abstractmethod
-    def cancel(self):
-        """
-        Cancel all tasks managed by this TaskManager
-        """
-        pass
-
-    
-    
-async def entangle(tasks, propagate=True, logger=None):
-    """
-    Entangle the fates of a group of tasks,
-    ensuring all are cancelled if one fails or is cancelled.
-    """
-    logger = logger or logging.getLogger(__name__)
-    try:
-        return await asyncio.gather(*tasks)
-    except Exception:
-        for t in tasks:
-            logger.debug("Cancelling task: {}".format(t))
-            t.cancel()
-        if propagate:
-            raise
-
-
-async def link(task1, task2, propagate=True, logger=None):
-    """
-    Create a unidirectional link between two tasks,
-    such that an exception raised in task2 
-    guarantees cancellation of task1.
-    """
-    logger = logger or logging.getLogger(__name__)
-    try:
-        await task2
-    except:
-        logger.info("link triggered on task {} following exception from task {}".format(task1, task2))
-        task1.cancel()
-        if propagate:
-            raise
-
-        
-class MultiError(Exception):
-    def __init__(self, exceptions):
-        self._exceptions = exceptions
-
-
-import enum
-
-
-class TaskGroupState(enum.Enum):
-    OPEN = enum.auto()
-    CLOSED = enum.auto()
-
-
-class TaskGroupClosedError(Exception):
-    def __init__(self, task_group):
-        super().__init__("Invalid operation on closed TaskGroup")
-        self.task_group = task_group
-
-    
-class TaskGroup(TaskManager):
-    """An object that keeps track of spawned tasks"""
-
-    def __init__(self, tasks=(), timeout=None, logger=None, loop=None, name=None):
-        self.name = name or str(id(self))
-        self._tasks = set(tasks)
-        self.logger = logger or logging.getLogger(
-            __name__ + "." + type(self).__name__ + "." + self.name
-        )
-        self.loop = loop or asyncio.get_event_loop()
-        self._state = TaskGroupState.OPEN
-        self._timeout = timeout
-
-    @property
-    def tasks(self):
-        return frozenset(self._tasks)
-
-    def __contains__(self, t: asyncio.Task):
-        return t in self._tasks
-
-    def add_tasks(self, *tasks):
-        """Add existing tasks to this TaskGroup."""
-        if self._state is TaskGroupState.CLOSED:
-            raise TaskGroupClosedError(self)
-        self._tasks.update(tasks)
-
-    def remove_tasks(self, *tasks):
-        """
-        Remove tasks from this task group.
-        This does not cancel those tasks.
-        """
-        if self._state is TaskGroupState.CLOSED:
-            raise TaskGroupClosedError(self)
-        self._tasks.difference_update(tasks)
-
-    def spawn(self, coro):
-        """
-        Spawn a task from a coroutine, into this task group.
-        """
-        if self._state is TaskGroupState.CLOSED:
-            raise TaskGroupClosedError(self)
-        task = self.loop.create_task(coro)
-        self._tasks.add(task)
-        return task
-
-    def __await__(self):
-        # wait for all tasks to complete
-        # yield from asyncio.wait(self._tasks, return_when=asyncio.ALL_COMPLETED).__await__()
-        # # identify tasks that failed(or were cancelled)
-        # exceptions = [
-        #     asyncio.CancelledError if t.cancelled() else t.exception()
-        #     for t in self._tasks
-        #     if t.cancelled() or t.exception() is not None
-        # ]
-        # if len(exceptions):
-        #     raise MultiError(exceptions)
-        # else:
-        # just raise on first task error, else return task results
-        if self._timeout is None:
-            yield from asyncio.gather(*self._tasks).__await__()
-        else:
-            yield from asyncio.wait_for(asyncio.gather(*self._tasks), self._timeout).__await__()
-
-    async def __aiter__(self):
-        for t in self._tasks:
-            yield (await t)
-
-    def __iter__(self):
-        for t in self._tasks:
-            yield t.result()
-
-    def cancel(self):
-        """
-        Cancel all tasks in this task group
-        """
-        for t in self._tasks:
-            t.cancel()
-            
-    async def __aenter__(self):
-        if self._state is TaskGroupState.CLOSED:
-            raise TaskGroupClosedError(self)
-        return self
-
-    async def __aexit__(self, exc_type, exc_value, exc_tb):
-        self._state = TaskGroupState.CLOSED
-        if exc_value is not None:
-            self.cancel()
-        else:
-            try:
-                await self
-            except:
-                self.cancel()
-                raise
-
-    def __len__(self):
-        return len(self._tasks)
-
-    @property
-    def status(self):
-        return self._state
-
-    def __repr__(self):
-        return f"{type(self).__name__}(name={self.name}, size={len(self._tasks)}, status={self._state})"
 
 
 
